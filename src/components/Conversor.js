ea7b876d8367f95f0f01d14aa1e5e9a6716302f4
Thiago Teixeira <thiagoemecal@gmail.com>
2020-11-07 20:44:07 -0300

    Teste

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Conversor extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <p>aaaaaaaaaaa</p>
+      </div>
+    );
   }
 }
 
